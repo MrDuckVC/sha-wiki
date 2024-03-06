c6a5a4eb338cf42ff0ef2717d2118ffda3dec1ef
MrDuckVC <cunev2005@gmail.com>
2024-03-06 20:40:49 +0200

    Remake share button for corporations.

--- backend/search_engine/static/search_engine/js/share_button.js
+++ backend/search_engine/static/search_engine/js/share_button.js
@@ -14,10 +14,20 @@ $("#shareCorporationModal").on("show.bs.modal", function (e) {
     modal.find(".modal-title").text("Share " + corporation_name + " (ID: " + corporation_id + ")");
 
     // Set the href of the share buttons.
-    modal.find(".modal-body #share-in-telegram").attr("href", "https://t.me/share/url?url=" + corporation_url);
-    modal.find(".modal-body #share-in-whatsapp").attr("href", "https://api.whatsapp.com/send?text=" + corporation_url);
+    // LINE
+    modal.find(".modal-body #share-in-line").attr("href", "https://social-plugins.line.me/lineit/share?url=" + corporation_url);
+    // Email
+    modal.find(".modal-body #share-in-email").attr("href", "mailto:?subject=Check out " + corporation_name + "&body=" + corporation_url);
+    // X (Twitter)
+    modal.find(".modal-body #share-in-x-twitter").attr("href", "https://twitter.com/intent/tweet?url=" + corporation_url);
+    // Instagram
+    modal.find(".modal-body #share-in-instagram").attr("href", "https://www.instagram.com/?url=" + corporation_url);
+    // Facebook
     modal.find(".modal-body #share-in-facebook").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + corporation_url);
-    modal.find(".modal-body #share-in-twitter").attr("href", "https://twitter.com/intent/tweet?url=" + corporation_url);
+    // WhatsApp
+    modal.find(".modal-body #share-in-whatsapp").attr("href", "https://api.whatsapp.com/send?text=" + corporation_url);
+    // Telegram
+    modal.find(".modal-body #share-in-telegram").attr("href", "https://t.me/share/url?url=" + corporation_url);
 
     // Set the value of the input field.
     modal.find("#share-link").val(corporation_url);
